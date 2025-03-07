395891bfef53a4cde613209aed8ae18ebcac6bad
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2025-03-07 18:30:27 +0000

    texto introduccion

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const artworkImages = document.querySelectorAll('.artwork-image');
+    const readMoreBtn = document.getElementById('read-more-btn');
+    const textContainer = document.querySelector('.text-container');
 
     artworkImages.forEach(image => {
         image.addEventListener('click', function() {
@@ -7,4 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = `src/pages/obra.html?id=${artworkId}`;
         });
     });
+    
+    readMoreBtn.addEventListener('click', function() {
+        textContainer.classList.toggle('expanded');
+        if (textContainer.classList.contains('expanded')) {
+            readMoreBtn.textContent = 'Leer menos';
+        } else {
+            readMoreBtn.textContent = 'Leer más';
+        }
+    });
+
 });
