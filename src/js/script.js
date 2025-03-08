c615f16afaeac9e4b20227e4dc7c45f13b9c2349
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2025-03-08 11:57:44 +0000

    cinta funcionando bien responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    //constante para la carga de las imagenes al pinchar en la obra
     const artworkImages = document.querySelectorAll('.artwork-image');
+    //constante para el boton de leer mas
     const readMoreBtn = document.getElementById('read-more-btn');
     const textContainer = document.querySelector('.text-container');
 
+    const marqueeContent = document.querySelector(".marquee-content");
+    const spans = marqueeContent.querySelectorAll("span");
+
+    //funcion para la carga de las imagenes al pinchar en la obra
     artworkImages.forEach(image => {
-        image.addEventListener('click', function() {
+        image.addEventListener('click', function () {
             const artworkId = this.dataset.id;
             window.location.href = `src/pages/obra.html?id=${artworkId}`;
         });
     });
-    
-    readMoreBtn.addEventListener('click', function() {
+    //funcion para contenedor de texto
+    readMoreBtn.addEventListener('click', function () {
         textContainer.classList.toggle('expanded');
         if (textContainer.classList.contains('expanded')) {
             readMoreBtn.textContent = 'Leer menos';
@@ -18,5 +24,18 @@ document.addEventListener('DOMContentLoaded', function() {
             readMoreBtn.textContent = 'Leer más';
         }
     });
+
+    //funcion para el marquee
+    let totalWidth = 0;
+    spans.forEach((span) => {
+        totalWidth += span.offsetWidth + parseInt(window.getComputedStyle(span).marginRight);
+    });
+
+    marqueeContent.innerHTML += marqueeContent.innerHTML;
+
+    const speed = 100; // Ajusta este valor para cambiar la velocidad
+    const duration = totalWidth / speed;
+
+    marqueeContent.style.animationDuration = `${duration}s`;
 
 });
